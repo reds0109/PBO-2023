3838159a7f75c552b5483298df4221781ea90f93
Reza Danis <148847358+reds0109@users.noreply.github.com>
2023-10-26 13:09:08 +0700

    Add files via upload

---
+++ Tugas 7 UTS/Buku.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pbo_uts;
+
+/**
+ *
+ * @author reza danis
+ */
+public class Buku {
+    private String isbn;
+    private String judul_buku;
+    private String tahun_terbit;
+    private String penerbit;
+
+    /**
+     * @return the isbn
+     */
+    public String getIsbn() {
+        return isbn;
+    }
+
+    /**
+     * @param isbn the isbn to set
+     */
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    /**
+     * @return the judul_buku
+     */
+    public String getJudul_buku() {
+        return judul_buku;
+    }
+
+    /**
+     * @param judul_buku the judul_buku to set
+     */
+    public void setJudul_buku(String judul_buku) {
+        this.judul_buku = judul_buku;
+    }
+
+    /**
+     * @return the tahun_terbit
+     */
+    public String getTahun_terbit() {
+        return tahun_terbit;
+    }
+
+    /**
+     * @param tahun_terbit the tahun_terbit to set
+     */
+    public void setTahun_terbit(String tahun_terbit) {
+        this.tahun_terbit = tahun_terbit;
+    }
+
+    /**
+     * @return the penerbit
+     */
+    public String getPenerbit() {
+        return penerbit;
+    }
+
+    /**
+     * @param penerbit the penerbit to set
+     */
+    public void setPenerbit(String penerbit) {
+        this.penerbit = penerbit;
+    }
+}
