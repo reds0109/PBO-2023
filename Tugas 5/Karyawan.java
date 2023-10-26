ef79e962f7ddd18054eb174bd6708d201c644bcc
Reza Danis <148847358+reds0109@users.noreply.github.com>
2023-10-26 12:59:06 +0700

    Add files via upload

---
+++ Tugas 5/Karyawan.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pbo_pertemuan5;
+
+/**
+ *
+ * @author reza danis
+ */
+public class Karyawan {
+    private String id_karyawan;
+    private String nama;
+    private String divisi;
+    private String domisili;
+
+    /**
+     * @return the id_karyawan
+     */
+    public String getId_karyawan() {
+        return id_karyawan;
+    }
+
+    /**
+     * @param id_karyawan the id_karyawan to set
+     */
+    public void setId_karyawan(String id_karyawan) {
+        this.id_karyawan = id_karyawan;
+    }
+
+    /**
+     * @return the nama
+     */
+    public String getNama() {
+        return nama;
+    }
+
+    /**
+     * @param nama the nama to set
+     */
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    /**
+     * @return the divisi
+     */
+    public String getDivisi() {
+        return divisi;
+    }
+
+    /**
+     * @param divisi the divisi to set
+     */
+    public void setDivisi(String divisi) {
+        this.divisi = divisi;
+    }
+
+    /**
+     * @return the domisili
+     */
+    public String getDomisili() {
+        return domisili;
+    }
+
+    /**
+     * @param domisili the domisili to set
+     */
+    public void setDomisili(String domisili) {
+        this.domisili = domisili;
+    }
+ 
+}
